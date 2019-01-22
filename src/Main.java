bf321b823948c5e54cdd61d31d70fbd3d1137583
Eugene <eugenesny@icloud.com>
2019-01-22 17:44:01 +0200

    update

--- src/Main.java
+++ src/Main.java
@@ -4,21 +4,28 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-    while(true) {
-        System.out.println("please enter a number to check if prime");
-        int num = in.nextInt();
+    Scanner in = new Scanner(System.in);
+    public void showPrime(){
+
+        while(true) {
+            System.out.println("please enter a number to check if prime");
+            int num = in.nextInt();
 
 
-        if (num % 2 == 0) {
-            System.out.println("Not a prime number");
-        } else if (num % 2 == 1) {
-            System.out.println("Prime Number");
-        } else if (num == 1) {
-            System.out.println("Not Prime number");
+            if (num % 2 == 0) {
+                System.out.println("Not a prime number");
+            } else if (num % 2 == 1) {
+                System.out.println("Prime Number");
+            } else if (num == 1) {
+                System.out.println("Not Prime number");
+            }
+
         }
-
     }
+
+    public static void main(String[] args) {
+        Main inst1 = new Main();
+        inst1.showPrime();
+
     }
 }
